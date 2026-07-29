2c5254329647b7213b4d05ad74317fa7547395e6
João R. Campos <jrcampos@dei.uc.pt>
2026-07-29 21:20:27 +0100

    Feature/deployment (#25)

--- application/preview_functinoality.sh
+++
@@ -1,5 +0,0 @@
-export PREVIEW_ID=42
-export COMPOSE_PROJECT_NAME=pr42
-export VITE_ORCHESTRATOR_API_ENDPOINT=/preview/42/chat
-
-../scripts/run-application.sh application/docker-compose.preview.yaml

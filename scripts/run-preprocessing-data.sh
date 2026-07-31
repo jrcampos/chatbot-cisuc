63485fb48a28cc8142f61fb8e064e209e737eb8a
jrcampos <jrcampos@dei.uc.pt>
2026-08-01 00:41:45 +0100

    DVC

--- scripts/run-preprocessing-data.sh
+++ scripts/run-preprocessing-data.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+set -a
+source config/chatbot-common.env
+source config/preprocessing.env
+set +a
+
 ./scripts/run-ingestion.sh
 ./scripts/run-enhancement.sh
 ./scripts/run-embeddings.sh
